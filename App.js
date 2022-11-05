5ea2f9553fde61a267474bbdbd53cfa5533d2144
nigussolomon <nigussolomon444@gmail.com>
2022-11-05 06:22:52 +0300

    added navigation and screens

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import PasswordReset from "./components/PasswordReset";
+
+const Stack = createNativeStackNavigator();
+const RegisterScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Register navigation={navigation}></Register>
+    </View>
+  );
+};
+
+const LoginScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Login navigation={navigation}></Login>
+    </View>
+  );
+};
+
+const PasswordResetScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <PasswordReset navigation={navigation}></PasswordReset>
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ title: "Register" }}
+        />
+
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Login" }}
+        />
+
+        <Stack.Screen
+          name="PasswordReset"
+          component={PasswordResetScreen}
+          options={{ title: "Password Reset" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
 });
