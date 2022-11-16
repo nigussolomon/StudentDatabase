202625b98ba0ebe3815470435b10c0d720022bfc
nigussolomon <nigussolomon444@gmail.com>
2022-11-16 19:38:49 +0300

    added all the required layouts with basic text component

---
+++ layouts/profile.js
@@ -0,0 +1,30 @@
+import { Stack, Text, Button, TextInput} from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import { View } from "react-native";
+
+export default function Profile() {
+  return (
+    <Stack spacing={10}>
+      <Text style={styles.headTxt} variant="h4">
+        Profile
+      </Text>
+    </Stack>
+  );
+}
+
+const styles = {
+  headTxt: {
+    fontWeight: "900",
+    color: "#00B822",
+  },
+
+  btn: {
+    color: "#ffffff",
+    padding: 4,
+    marginTop: 10,
+  },
+
+  btnView: {
+    flexDirection: 'row',
+  }
+};
